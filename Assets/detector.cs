2b1c2665cdfd19c1eb94e69edc9adabed3442cb3
dmtr <kabanov.dmitry.vladimirovich@gmail.com>
2019-11-22 11:12:10 +0300

    Initial commit

---
+++ Assets/detector.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class detector : MonoBehaviour
+{
+    int score = 0;
+    public GameObject pref;
+    public GameObject spawnPoint;
+    public GameObject scoreText;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        score++;
+        Destroy(other.gameObject);
+        GameObject buffer = Instantiate<GameObject>(pref);
+        buffer.transform.position = spawnPoint.transform.position;
+        scoreText.GetComponent<TextMesh>().text = score.ToString();
+    }
+}
